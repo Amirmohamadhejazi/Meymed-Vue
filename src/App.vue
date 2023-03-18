2167e499e2b0713fcbd406f18a3746919883b93f
Amirmohamadhejazi <Amirmohamadhejazi.9731@gmail.com>
2023-03-18 18:12:54 +0330

    initial cmt && header ok

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<!--  <nav>-->
+<!--    <RouterLink to="/">Home</RouterLink>-->
+<!--    <RouterLink to="/about">About</RouterLink>-->
+<!--  </nav>-->
+  <RouterView />
+</template>
+<script>
+import { RouterView } from 'vue-router'
+// import { RouterLink, RouterView } from 'vue-router'
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  components:{
+    // HelloWorld,
+    // RouterLink,
+    RouterView
+  }
+}
+</script>
